c8ffb9faccc2cf5316974d6a1beb1229d6644dab
Richard Fan <>
2021-03-15 21:34:59 +0800

    Init

---
+++ my-module/projects/A/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  # backend "remote" {
+  #   organization = "richardfan1126"
+
+  #   workspaces {
+  #     name = "test"
+  #   }
+  # }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
