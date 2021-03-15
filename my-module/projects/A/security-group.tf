399a3b1cbfc9c6d8ec27727e2a8fde1c0ca5bfe2
Richard Fan <>
2021-03-15 21:43:21 +0800

    .

--- my-module/projects/A/security-group.tf
+++ my-module/projects/A/security-group.tf
@@ -1,9 +1,9 @@
-module "security-group" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "3.18.0"
+# module "security-group" {
+#   source  = "terraform-aws-modules/security-group/aws"
+#   version = "3.18.0"
 
-  name                = "my-security-group"
-  vpc_id              = "vpc-05c07e7f"
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["https-443-tcp", "http-80-tcp"]
-}
+#   name                = "my-security-group"
+#   vpc_id              = "vpc-05c07e7f"
+#   ingress_cidr_blocks = ["0.0.0.0/0"]
+#   ingress_rules       = ["https-443-tcp", "http-80-tcp"]
+# }
